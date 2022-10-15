62e820d753afda1bf1707dbc72d6187195e8a2c6
JustinFagg <110573735+JustinFagg@users.noreply.github.com>
2022-10-15 23:38:19 +1000

    code 90% commented before anyone checks

--- Code/main.c
+++ Code/main.c
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include "hardware/pwm.h"
 #include "hardware/uart.h"
 #include "terminal.h"
@@ -44,7 +45,8 @@ volatile unsigned int indexx = 0;
 #define SW3 17
 #define SW4 16
 
-int one_step_in_dir(int move, int step, int dir) // When using this one need to set both dir and step to false after a sleep command
+// When using this one need to set both dir and step to false after a sleep command, returns 1 or -1 depending on direction.
+int one_step_in_dir(int move, int step, int dir)
 {
   if (move > 0)
   {
@@ -63,9 +65,10 @@ int one_step_in_dir(int move, int step, int dir) // When using this one need to 
   }
 }
 
-int debug_step(step, dir) // When using this one no need to set any sleep command
+// When using this one no need to set any sleep command, returns 1 or -1 depending on direction.
+int debug_step(int step, int dir)
 {
-  if ((!gpio_get(SW4)) && (!gpio_get(SW3)))
+  if ((!gpio_get(SW4)) && (!gpio_get(SW3))) // negate both buttons pushed
   {
     return 0;
   }
@@ -93,6 +96,7 @@ int debug_step(step, dir) // When using this one no need to set any sleep comman
   }
 }
 
+// Blinks green light amount times
 #define blink(amount)                    \
   {                                      \
     for (int i = 0; i < (codeType); i++) \
@@ -104,7 +108,7 @@ int debug_step(step, dir) // When using this one no need to set any sleep comman
     }                                    \
   }
 
-// Main step code, from having a set move of x y and z it will straightline the dzired amount.
+// Main step code, moves until x_move, y_move and z_move all equil zero, and in straight line.
 #define moving_steps()                                             \
   {                                                                \
     while (abs(x_move) + abs(y_move) + abs(z_move) != 0)           \
@@ -170,8 +174,6 @@ int debug_step(step, dir) // When using this one no need to set any sleep comman
 
 int main(void)
 {
-  ////////////////////////////////////////////////////
-
   // Initialise components and variables
   stdio_init_all();
   gpio_init(Spindle);
@@ -249,7 +251,7 @@ int main(void)
   int cur_step = 0;
 
   // currently input of points to follow as x, y, z, repeated
-  //  static int list[] = {0, 190, 30, 0, 187, 32, 0, 184, 35, 0, 181, 37, 0, 178, 40, 0, 175, 43, 0, 170, 45, 0, 166, 47, 0, 162, 50, 0, 155, 54, 0, 151, 56, 0, 147, 58, 0, 143, 61, 0, 138, 63, 0, 132, 65, 0, 128, 68, 0, 124, 72, 0, 124, 68, 0, 124, 63, 0, 122, 60, 0, 121, 62, 0, 122, 65, 0, 122, 67, 0, 119, 68, 0, 116, 66, 0, 110, 66, 0, 105, 67, 0, 99, 68, 0, 95, 68, 0, 94, 62, 0, 92, 65, 0, 93, 68, 0, 95, 72, 0, 92, 70, 0, 89, 67, 0, 83, 65, 0, 80, 63, 0, 75, 60, 0, 70, 57, 0, 65, 54, 0, 62, 51, 0, 57, 46, 0, 52, 44, 0, 47, 43, 0, 42, 40, 0, 39, 37, 0, 37, 34, 0, 28, 32, 0, 29, 36, 0, 32, 41, 0, 34, 47, 0, 30, 45, 0, 35, 52, 0, 30, 54, 0, 38, 62, 0, 33, 66, 0, 47, 71, 0, 42, 75, 0, 55, 79, 0, 52, 82, 0, 62, 86, 0, 58, 89, 0, 72, 90, 0, 69, 94, 0, 82, 94, 0, 82, 98, 0, 92, 94, 0, 90, 91, 0, 86, 88, 0, 78, 88, 0, 72, 84, 0, 66, 81, 0, 63, 79, 0, 58, 75, 0, 55, 73, 0, 50, 67, 0, 44, 62, 0, 42, 57, 0, 42, 48, 0, 47, 52, 0, 53, 56, 0, 60, 60, 0, 65, 64, 0, 70, 67, 0, 74, 71, 0, 82, 74, 0, 90, 78, 0, 95, 85, 0, 95, 83, 0, 96, 79, 0, 96, 76, 0, 100, 73, 0, 103, 73, 0, 107, 76, 0, 111, 76, 0, 114, 74, 0, 118, 73, 0, 123, 79, 0, 124, 83, 0, 118, 86, 0, 112, 87, 0, 109, 91, 0, 106, 88, 0, 103, 88, 0, 97, 88, 0, 94, 88, 0, 94, 90, 0, 94, 96, 0, 96, 100, 0, 97, 105, 0, 95, 111, 0, 91, 116, 0, 95, 114, 0, 95, 121, 0, 102, 116, 0, 104, 126, 0, 107, 118, 0, 111, 125, 0, 114, 117, 0, 120, 124, 0, 119, 113, 0, 125, 119, 0, 119, 106, 0, 119, 98, 0, 116, 106, 0, 110, 108, 0, 105, 109, 0, 101, 106, 0, 97, 97, 0, 100, 101, 0, 105, 105, 0, 110, 106, 0, 115, 103, 0, 117, 101, 0, 120, 97, 0, 122, 91, 0, 125, 88, 0, 132, 86, 0, 138, 85, 0, 143, 83, 0, 154, 81, 0, 159, 75, 0, 163, 71, 0, 167, 66, 0, 170, 61, 0, 172, 54, 0, 172, 50, 0, 173, 46, 0, 180, 39, 0, 186, 33, 0, 185, 46, 0, 188, 46, 0, 182, 57, 0, 187, 56, 0, 176, 67, 0, 181, 68, 0, 167, 76, 0, 172, 79, 0, 154, 81, 0, 160, 85, 0, 140, 89, 0, 147, 92, 0, 131, 92, 0, 137, 96, 0, 121, 97};
+  ////  static int list[] = {0, 190, 30, 0, 187, 32, 0, 184, 35, 0, 181, 37, 0, 178, 40, 0, 175, 43, 0, 170, 45, 0, 166, 47, 0, 162, 50, 0, 155, 54, 0, 151, 56, 0, 147, 58, 0, 143, 61, 0, 138, 63, 0, 132, 65, 0, 128, 68, 0, 124, 72, 0, 124, 68, 0, 124, 63, 0, 122, 60, 0, 121, 62, 0, 122, 65, 0, 122, 67, 0, 119, 68, 0, 116, 66, 0, 110, 66, 0, 105, 67, 0, 99, 68, 0, 95, 68, 0, 94, 62, 0, 92, 65, 0, 93, 68, 0, 95, 72, 0, 92, 70, 0, 89, 67, 0, 83, 65, 0, 80, 63, 0, 75, 60, 0, 70, 57, 0, 65, 54, 0, 62, 51, 0, 57, 46, 0, 52, 44, 0, 47, 43, 0, 42, 40, 0, 39, 37, 0, 37, 34, 0, 28, 32, 0, 29, 36, 0, 32, 41, 0, 34, 47, 0, 30, 45, 0, 35, 52, 0, 30, 54, 0, 38, 62, 0, 33, 66, 0, 47, 71, 0, 42, 75, 0, 55, 79, 0, 52, 82, 0, 62, 86, 0, 58, 89, 0, 72, 90, 0, 69, 94, 0, 82, 94, 0, 82, 98, 0, 92, 94, 0, 90, 91, 0, 86, 88, 0, 78, 88, 0, 72, 84, 0, 66, 81, 0, 63, 79, 0, 58, 75, 0, 55, 73, 0, 50, 67, 0, 44, 62, 0, 42, 57, 0, 42, 48, 0, 47, 52, 0, 53, 56, 0, 60, 60, 0, 65, 64, 0, 70, 67, 0, 74, 71, 0, 82, 74, 0, 90, 78, 0, 95, 85, 0, 95, 83, 0, 96, 79, 0, 96, 76, 0, 100, 73, 0, 103, 73, 0, 107, 76, 0, 111, 76, 0, 114, 74, 0, 118, 73, 0, 123, 79, 0, 124, 83, 0, 118, 86, 0, 112, 87, 0, 109, 91, 0, 106, 88, 0, 103, 88, 0, 97, 88, 0, 94, 88, 0, 94, 90, 0, 94, 96, 0, 96, 100, 0, 97, 105, 0, 95, 111, 0, 91, 116, 0, 95, 114, 0, 95, 121, 0, 102, 116, 0, 104, 126, 0, 107, 118, 0, 111, 125, 0, 114, 117, 0, 120, 124, 0, 119, 113, 0, 125, 119, 0, 119, 106, 0, 119, 98, 0, 116, 106, 0, 110, 108, 0, 105, 109, 0, 101, 106, 0, 97, 97, 0, 100, 101, 0, 105, 105, 0, 110, 106, 0, 115, 103, 0, 117, 101, 0, 120, 97, 0, 122, 91, 0, 125, 88, 0, 132, 86, 0, 138, 85, 0, 143, 83, 0, 154, 81, 0, 159, 75, 0, 163, 71, 0, 167, 66, 0, 170, 61, 0, 172, 54, 0, 172, 50, 0, 173, 46, 0, 180, 39, 0, 186, 33, 0, 185, 46, 0, 188, 46, 0, 182, 57, 0, 187, 56, 0, 176, 67, 0, 181, 68, 0, 167, 76, 0, 172, 79, 0, 154, 81, 0, 160, 85, 0, 140, 89, 0, 147, 92, 0, 131, 92, 0, 137, 96, 0, 121, 97};
   static int list[] = {0, 0, 0, 0, 1, 3.5, 0, 5, 4, 0, 7, 5, 0, 12, 10, 0, 13, 12, 0, 8, 11, 0, 4, 10, 0, -1, 7, 0, -2, 9, 0, -2.5, 8, 0, -3, 10, 0, -3.5, 9, 0, -5, 11, 0, -6, 10, 0, -8, 11, 0, -8, 9, 0, -9.5, 10, 0, -10, 8, 0, -11, 12, 0, -11, 15, 0, -14, 18, 0, -15.5, 20, 0, -16, 18, 0, -16, 13, 0, -14, 8, 0, -12, 6, 0, -13, 0, 0, -12, -2, 0, -9, -4, 0, -6, -5, 0, -2, -4, 0, 0, -1, 0, 0, 0, 0, 0, -1, 0, -2, -4, 0, -6, -5, 0, -9, -4, 0, -12, -2, 0, -13, 0, 0, -15, -2, 0, -15, -5, 0, -13, -9, 0, -12, -11, 0, -7, -13, 0, -2, -13, 0, 1, -11, 0, 3, -5, 0, 2, -2, 0, 0, -1, 0, 2, -2, 0, 6, 3, 0, 10, 4, 0, 12, 5, 0, 15, 8, 0, 16, 3, 0, 15.5, 0, 0, 14, -5, 0, 11, -8, 0, 8, -9, 0, 5, -9, 0, 3, -7, 0, 5, -9, 0, 8, -9, 0, 11, -8, 0, 14, -5, 0, 15.5, 0, 0, 16, 3, 0, 15, 8, 0, 12, 5, 0, 10, 4, 0, 6, 3, 0, 2, -2, 0, 0, -1, 0, 2, -2, 0, 3, -5, 0, 1, -11, 0, 0.5, -11.5, 0, -1, -15, 0, -2, -17, 0, -3.5, -17, 0, -2, -17, 0, -1, -15, 0, 0.5, -11.5, 0, 1, -11, 0, 1, -15, 0, 0, -17, 0, 0, -18, 0, 2, -18, 0, 4, -17, 0, 5, -14, 0, 5, -9, 0, 5, -14, 0, 4, -17, 0, 2, -18, 0, 0, -18, 0, 0, -17, 0, 1, -15, 0, 1, -11, 0, -2, -13, 0, -3, -15, 0, -4, -18, 0, -6, -19.5, 0, -8, -18.5, 0, -7, -16, 0, -8, 18.5, 0, -9, -19, 0, -11, -18, 0, -10.5, -16, 0, -11, -13, 0, -11, -12};
   // makes set length of xyz list
   int xlist[sizeof(list) / 3];
@@ -269,7 +271,7 @@ int main(void)
 
   while (true)
   {
-    while ((codeType < 1) || (codeType > 4))//selects the codetype using the onboard buttons.
+    while ((codeType < 1) || (codeType > 4)) // selects the codetype using the onboard buttons.
     {
       if (!gpio_get(SW1) + !gpio_get(SW2) + !gpio_get(SW3) + !gpio_get(SW4) > 1)
       {
@@ -293,41 +295,42 @@ int main(void)
       }
       blink(codeType)
     }
-    while (codeType == 1)//debug using physical buttons.
+    while (codeType == 1) // debug using physical buttons.
     {
       int debug = 1;
-      while (debug == 1)
+      while (debug == 1) // debug = 0 usedd to fall outof this loop, if codetype still 1 by end debug reactivated.
       {
-        if (!gpio_get(SW2))
+        if (!gpio_get(SW2)) // changes what motor is being affected, (1-4)loop
         {
           debug_target++;
-          debug_target = ((debug_target) > (3) ? (0) : debug_target);
+          debug_target = ((debug_target) > (3) ? (0) : debug_target); // debug_target = (if (debug_target > 3) {0} else {debug_target})
           blink(debug_target + 1)
         }
         if (!gpio_get(SW1))
         {
+          // TODO add fall out of debug/settings
           // options
         }
-        // TODO add fall out of debug/settings
-        if (debug_target == 0)
+        // All the below test if SW3 or SW4 have been clicked in them, chose to make debug_target be main factor because of 3 the spinndle
+        if (debug_target == 0) // xstepper mottor.
         {
           x_place = x_place + debug_step(Xstep, Xdir);
         }
-        if (debug_target == 1)
+        if (debug_target == 1) // ystepper mottor.
         {
           y_place = y_place + debug_step(Ystep, Ydir);
         }
-        if (debug_target == 2)
+        if (debug_target == 2) // zstepper mottor.
         {
           z_place = z_place + debug_step(Zstep, Zdir);
         }
-        if (debug_target == 3)
+        if (debug_target == 3) // Spindle mottor.
         {
           //////TODO turn on the spindle TODO
         }
       }
     }
-    while (codeType == 2)//Runs through list of cordanates.
+    while (codeType == 2) // Runs through list of cordanates.
     {
       x_move = xlist[cur_step] - x_place;
       y_move = ylist[cur_step] - y_place;
@@ -335,9 +338,9 @@ int main(void)
       cur_step++;
       moving_steps()
     }
-    while (codeType == 3)//Terminal
+    while (codeType == 3) // Terminal
     {
-      if (write == 1)
+      if (write == 1) // Prints terminal
       {
         term_set_color(clrBlack, clrBlack);
         term_move_to(2, 16);
@@ -402,9 +405,10 @@ int main(void)
         term_move_to(1, 15);
         printf("Command prompt:\n> ");
         write = 0;
+        // TODO make second page of commands and a way to get there!
       }
-      int ch = getchar_timeout_us(0);
-      if ((ch == '\r') || (ch == '\n') || (indexx == 99))
+      int ch = getchar_timeout_us(0);                     // gets key pressed
+      if ((ch == '\r') || (ch == '\n') || (indexx == 99)) // runs command.
       {
         buffer[indexx] = 0;
         indexx = 0;
@@ -413,11 +417,12 @@ int main(void)
         int volatile valy = 0;
         int volatile valz = 0;
         int volatile val = 0;
-        if (sscanf(buffer, "goto %d %d %d", &valx, &valy, &valz) > 0)
+        if (sscanf(buffer, "goto %d %d %d", &valx, &valy, &valz) > 0) // goto command makes it move to the set step possition.
         {
           x_move = valx - x_place;
           y_move = valy - y_place;
           z_move = valz - z_place;
+          // echos command
           term_move_to(3, 10);
           term_erase_line();
           term_move_to(3, 9);
@@ -425,6 +430,7 @@ int main(void)
           printf("Move");
           term_move_to(2, 10);
           printf("[%d, %d, %d]", x_move, y_move, z_move);
+          // fixes the 2 lines that got cleared, not needed for small steps but long loading times need it for neatening.
           for (int i = 0; i < 2; i++)
           {
             term_set_color(clrBlack, clrYellow);
@@ -442,8 +448,9 @@ int main(void)
             printf(" \n ");
           }
         }
-        else if (sscanf(buffer, "move %d %d %d", &x_move, &y_move, &z_move) > 0)
+        else if (sscanf(buffer, "move %d %d %d", &x_move, &y_move, &z_move) > 0) // move the set amount from current possition.
         {
+          // echos command
           term_move_to(3, 10);
           term_erase_line();
           term_move_to(3, 9);
@@ -451,6 +458,7 @@ int main(void)
           printf("Move");
           term_move_to(2, 10);
           printf("[%d, %d, %d]", x_move, y_move, z_move);
+          // fixes the 2 lines that got cleared, not needed for small steps but long loading times need it for neatening.
           for (int i = 0; i < 2; i++)
           {
             term_set_color(clrBlack, clrYellow);
@@ -468,9 +476,9 @@ int main(void)
             printf(" \n ");
           }
         }
-        else if (sscanf(buffer, "codetype %d", &val) > 0)
+        else if (sscanf(buffer, "codetype %d", &val) > 0) // changes the codetype.
         {
-          if ((val >= 0) && (val <= 4))
+          if ((val >= 0) && (val <= 4)) // if legal value
           {
             term_cls();
             codeType = val;
@@ -493,12 +501,12 @@ int main(void)
           printf("Nothing run\n\r");
         }
 
-        //////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////
         moving_steps()
             write = 1;
         /////////////////////////////////////////////////////////////
       }
-      else if (ch == '\177')
+      else if (ch == '\177') // backspace.
       {
         if (indexx != 0)
         {
@@ -507,20 +515,20 @@ int main(void)
           printf("%c", ch);
         }
       }
-      else if (ch == '\f')
+      else if (ch == '\f') // not too sure about this think it is the clear terminal command.
       {
         buffer[indexx] = 0;
         indexx = 0;
         printf("%c", ch);
       }
-      else if (ch != -1)
+      else if (ch != -1) // if any other char print it and move to next buffer slot.
       {
         buffer[indexx] = ch;
         indexx++;
         printf("%c", ch);
       }
     }
-    while (codeType == 4)//testing area ATM, currently draws circles.
+    while (codeType == 4) // testing area ATM, currently draws circles.
     {
       for (int j = 100; j <= 2000; j = j + 100)
       {
